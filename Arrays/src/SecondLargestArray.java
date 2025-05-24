09e85780d3f81c66d5ee4b1e486c97796680dc83
Aditya Srivastava <aditya107161@gmail.com>
2025-05-24 19:19:25 +0530

    SecondLargestInArray Finding

---
+++ Arrays/src/SecondLargestArray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SecondLargestArray {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the Size of the Array");
+        int size = s.nextInt();
+        int[] array = new int[size];
+        System.out.println("Enter the elements in Array");
+        for (int i = 0; i < size; i++) {
+            array[i] = s.nextInt();
+        }
+        int firstMax = Integer.MIN_VALUE, secondMax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < size; i++) {
+            if (firstMax < array[i]) {
+                secondMax = firstMax;
+                firstMax = array[i];
+            }
+        }
+        System.out.println("SecondMax is " + secondMax);
+    }
+
+}
