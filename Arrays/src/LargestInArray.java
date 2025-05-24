07192ea0ca293ae78985f6c0e6cc61134786db82
Aditya Srivastava <aditya107161@gmail.com>
2025-05-24 19:10:11 +0530

    LargestInArray Finding

--- Arrays/src/LargestInArray.java
+++ Arrays/src/LargestInArray.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class LargestInArray {
     public static void main(String[] args) {
-
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the Size of the Array");
+        int size=s.nextInt();
+        int[] array =new int[size];
+        System.out.println("Enter the elements in Array");
+        for (int i = 0; i <size ; i++) {
+            array[i]=s.nextInt();
+        }
+        int max=Integer.MIN_VALUE;
+        for (int i = 0; i <size ; i++) {
+            max=Math.max(max,array[i]);
+        }
+        System.out.println("Max is " + max);
     }
 }
