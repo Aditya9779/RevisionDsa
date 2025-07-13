355d712567dafb5ad820eaa2451d36a61c14767b
Aditya Srivastava <aditya107161@gmail.com>
2025-07-13 23:54:47 +0530

    Added the Method That works for the Second Question also

--- BinarySearch/src/Easy/SearchInSortedArray1_Lc33.java
+++ BinarySearch/src/Easy/SearchInSortedArray1_Lc33.java
@@ -3,6 +3,41 @@ package Easy;
 public class SearchInSortedArray1_Lc33 {
     public static void main(String[] args) {
         System.out.println(new SearchInSortedArray1_Lc33().search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+        System.out.println("Main "+new SearchInSortedArray1_Lc33().searchMain(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+    }
+    //Comparing Best Method for Question 1,2 in LC
+        public int searchMain(int[] nums, int target) {
+            if(nums.length==0) return 0;
+            int start=0;
+            int end=nums.length-1;
+            while(start<=end){
+                int mid=(start+end)/2;
+                if(nums[mid]==target){
+                    return mid;
+                }
+                //Left Half
+                if(nums[start]<=nums[mid]){
+                    //Checking like the Target is in between the left and mid value
+                    //and if mid is greater then end end is minus-- Normal from BinarySearch
+                    //This (nums[start]<=target ) only addition condition
+                    if(nums[start]<=target && nums[mid]>=target){
+                        end=mid-1;
+                    }
+                    else{
+                        start=mid+1;
+                    }
+                }
+                //Right Half
+                else{
+                    if(nums[end]>=target && nums[mid]<=target){
+                        start=mid+1;
+                    }
+                    else{
+                        end=mid-1;
+                    }
+                }
+            }
+            return -1;
     }
 
     public int search(int[] nums, int target) {
